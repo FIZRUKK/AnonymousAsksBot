20870b4ed49436c0b546111020f54fa1d3e24bd1
FIZZZRUK <157704026+FIZRUKK@users.noreply.github.com>
2024-03-08 21:18:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
 TOKEN = '7085630150:AAERqPaUUd5wbF0qh-rPNO0GTChhManl02s'  # Замените на токен вашего бота
-ADMIN_ID = 2085376749  # Замените на ID вашего администратора
+ADMIN_ID = 2016568074  # Замените на ID вашего администратора
 DATABASE_PATH = 'database.db'  # Путь к файлу базы данных SQLite
